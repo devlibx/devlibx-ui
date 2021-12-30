bc19ff1ae7e61203439af937e33c91b69355da3c
Harish Bohara <harish.bohara@gmail.com>
2021-12-30 20:43:11 +0530

    added kafka consumer example

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from './components/Button/Button'
+import KafkaConsumer from './components/Kafka/KafkaConsumer'
 
 ReactDOM.render(
   <React.StrictMode> 
-    <Button/>      
+    <Button/> 
+    <KafkaConsumer 
+      kafk_consumer={
+        {
+          topic: "My_Topic",
+          brokers: "10.2.0.15:9092",
+          partitions: 11,
+          replication_factor: 4,
+          group_id: "my_group_id",
+          publish_message_timeout_ms: 1001,
+          acks: "all"
+        }
+      } />     
   </React.StrictMode>,
   document.getElementById('root')
 );
